1bf0955ac15fe867dd6bf05027c3227ce54f2387
LSmyrnaios <lsmyrnaios@gmail.com>
2024-09-05 22:18:17 +0300

    - Avoid losing some full-texts. - Update the "runExample.sh" script to use the newest version of the app. - Update the "sample_output.json" file to include the "fileHash" and "fileSize" parameters. - Code polishing.

--- runExample.sh
+++ runExample.sh
@@ -9,11 +9,7 @@ rm -rf example/sample_output/*
 # Run the program.
 cd target || exit
 
-command="java -jar publications_retriever-1.1-SNAPSHOT.jar -retrieveDataType all -downloadDocFiles -docFileNameType numberName -firstDocFileNum 1 -docFilesStorage ../example/sample_output/DocFiles < ../example/sample_input/sample_input.json > ../example/sample_output/sample_output.json"
+command="java -jar publications_retriever-1.2-SNAPSHOT.jar -retrieveDataType all -downloadDocFiles -docFileNameType numberName -firstDocFileNum 1 -docFilesStorage ../example/sample_output/DocFiles < ../example/sample_input/sample_input.json > ../example/sample_output/sample_output.json"
 echo -e "\nRunning: $command\n"
-
-# Unfortunately, the plain "$command" does not work ,so we have to re-type the commend..
-
-java -jar publications_retriever-1.1-SNAPSHOT.jar -retrieveDataType all -downloadDocFiles -docFileNameType numberName -firstDocFileNum 1 -docFilesStorage ../example/sample_output/DocFiles < ../example/sample_input/sample_input.json > ../example/sample_output/sample_output.json
-
+eval "$command"
 echo "Finished"
