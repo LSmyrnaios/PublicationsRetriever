f082f90e0a4f18cabf1172c33d8a56757409f07c
lsmyrnaios <lsmyrnaios@di.uoa.gr>
2025-07-14 17:15:33 +0300

    Add missing change.

--- src/main/java/eu/openaire/publications_retriever/util/http/ConnSupportUtils.java
+++ src/main/java/eu/openaire/publications_retriever/util/http/ConnSupportUtils.java
@@ -1294,7 +1294,7 @@ public class ConnSupportUtils
 			}
 
 			// Replace potential encoded '&' symbols which cause navigation issues inside the site.
-			currentLink = StringUtils.replace(currentLink, "amp;", "&", -1);
+			currentLink = Strings.CS.replace(currentLink, "amp;", "&", -1);
 
 			if ( currentLink.startsWith("?") )	// This case is mishandled by the automatic generation, unless we use this custom logic.
 				return (urlBase + currentLink);
